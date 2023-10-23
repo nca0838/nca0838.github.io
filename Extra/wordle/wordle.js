df212ff0d15baf3a033faa2ab93ebd40e5dec6c0
Nathaniel Atkinson <nca0838@ppsbstudent.org>
2023-10-23 02:36:48 +0000

    tiny changes

--- Extra/wordle/wordle.js
+++ Extra/wordle/wordle.js
@@ -204,27 +204,27 @@ if (wordPicker === 1){
   word = "BROAD"
 }else if (wordPicker === 99){
   word = "CHEAP"
-}else if (wordPicker === 2){
+}else if (wordPicker === 100){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 101){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 102){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 103){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 104){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 105){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 106){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 107){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 108){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 109){
   word = "WATER"
-}else if (wordPicker === 2){
+}else if (wordPicker === 110){
   word = "WATER"
 }else if (wordPicker === 2){
   word = "WATER"
@@ -351,10 +351,9 @@ window.onload = function(){
 
 function intialize() {
 
-    // Create the game board
+    // Creating the game board
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
-            // <span id="0-0" class="tile">P</span>
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
@@ -364,7 +363,7 @@ function intialize() {
     }
 
 
-    // Listen for Key Press
+    // Listening for Key Press
     document.addEventListener("keyup", (e) => {
         if (gameOver) return; 
 
